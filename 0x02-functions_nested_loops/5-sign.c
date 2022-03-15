fcfb43d03956e347b73644ddd806147e094135e8
Lawry03 <jasmwansa@gmail.com>
2022-03-15 14:32:32 -0700

    Created file name 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of the number
+ * @n: is the integer to check
+ * Return: 1 if greater than 0 and -1 if less than 0/
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
