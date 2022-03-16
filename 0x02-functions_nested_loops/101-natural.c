fbbfffe6bd030554b2c8ecd46326f40cf20a88a2
Lawry03 <jasmwansa@gmail.com>
2022-03-16 06:20:07 -0700

    Created file named 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - computes sum of all the multiples of 3 or 5 below 1024.
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	unsigned long int sum;
+	int i;
+
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum = sum + i;
+		}
+		else if ((I % 5) == 0)
+		{
+			sum = sum + 1;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
