a45dff7bd4fff83e342b83acaa6cf997bc6416b2
Lawry03 <jasmwansa@gmail.com>
2022-03-16 13:37:19 -0700

    Updated file content.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,17 +13,17 @@ int print_last_digit(int i)
 	if (i > 0)
 	{
 		last_digit = i % 10;
-		_putchar(last_digit);
+		_putchar(last_digit + '0');
 	}
 	else if (i == 0)
 	{
 		last_digit = i * 0;
-		_putchar(last_digit);
+		_putchar(last_digit + '0');
 	}
 	else
 	{
 		last_digit = (i % 10) * -1;
-		_putchar(last_digit);
+		_putchar(last_digit + '0');
 	}
 	return (last_digit);
 }
