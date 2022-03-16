f9ca294c323ccccb0c7f1da7f73285ec6ebc43d1
Lawry03 <jasmwansa@gmail.com>
2022-03-16 04:45:55 -0700

    Created file 100-times_tables.c

---
+++ 0x02-functions_nested_loops/100-times_tables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - Prints times tables of numbers between 0 and 15.
+ * @n: Input number.
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if ((n >= 0) && (n <= 15))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				printf("%d, ", i * j);
+			}
+			_putchar('\n');
+		}
+	}
+}
