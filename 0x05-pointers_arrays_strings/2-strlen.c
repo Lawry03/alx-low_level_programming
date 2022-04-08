4eac24fda385d4b6824d5e92176b2b74d866afe5
Lawry03 <jasmwansa@gmail.com>
2022-04-08 03:30:35 -0700

    Created file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - determines the length of a string
+ * @s: Pointer to the string.
+ * Return: Length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
